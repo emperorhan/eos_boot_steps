67fb8c4debbc7de0bc781f4587f444d60e990213
Emperorhan <iny27@naver.com>
2018-12-04 23:23:43 +0900

    init

---
+++ 05_SYSTEM_CONTRACTS.sh
@@ -0,0 +1,5 @@
+SOURCES_FOLDER="$( jq -r '.SOURCES_FOLDER' "00_CONFIG.conf" )"
+CONTRACTS_FOLDER="$SOURCES_FOLDER/build/contracts"
+
+./cleos.sh set contract eosio $CONTRACTS_FOLDER/eosio.system -p eosio
+
